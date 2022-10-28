58cf1879c925503f111c79070ba9417857dd22a3
Zhenya Korban <korbanzhenya@gmail.com>
2022-10-28 23:00:09 +0300

    vers.1

--- pyneng_3.0/gosha-python_IF.py
+++
@@ -1,3 +0,0 @@
-num = input("Your digit?\n")
-result = "Вы введи число большее 10ти" if int(num) > 10 else "False"
-print(result)
--- pyneng_3.0/pyneng_Tasks2_Types of data.py
+++
@@ -1,7 +0,0 @@
-print(1 + 2)
-print(1.0 + 2)
-print(10 - 4)
-print(2**3)
-
-
-
