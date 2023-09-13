9873c04ad7043b7e479c88adacbcf9575641e2e1
kevgenius <95285949+kevgenius@users.noreply.github.com>
2023-09-13 15:41:27 +0300

    Update Task6.1_.py

--- pyneng_3.0/Task6.1_.py
+++ pyneng_3.0/Task6.1_.py
@@ -5,9 +5,10 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX.
 список mac_cisco
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
-mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
+#mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 #Решение:
+mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 mac_cisco = []
 for address in mac:
     mac_cisco.append(address.replace(":", "."))
